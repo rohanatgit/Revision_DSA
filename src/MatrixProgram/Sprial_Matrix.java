c2afd0e363a937f04a0e8bf907456ee99be16428
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-07-28 15:25:54 +0530

    Matrix

--- src/MatrixProgram/Sprial_Matrix.java
+++ src/MatrixProgram/Sprial_Matrix.java
@@ -2,30 +2,39 @@ package MatrixProgram;
 
 public class Sprial_Matrix {
     public static void main(String[] args) {
-         //TODO AUTO-GENRATED MATHEROD STUB
-        int arr[][]={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+        //TODO AUTO-GENRATED MATHEROD STUB
+        int arr[][] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
         sprialmatrix(arr);
     }
-    public static void sprialmatrix(int arr[][]){
-        int minr=0;
-        int maxr=arr.length-1;
-        int minc=0;
-        int maxc=arr[0].length-1;
-        for(int i=minc;i<=maxc;i++){
-            System.out.println(arr[minr][i]+" ");
+
+    public static void sprialmatrix(int arr[][]) {
+        int minr = 0;
+        int maxr = arr.length - 1;
+        int minc = 0;
+        int maxc = arr[0].length - 1;
+        int total=arr[arr.length][arr[0].length];
+        int c=0;
+        while (c<total) {
+            for (int i = minc; i <= maxc; i++) {
+                System.out.print(arr[minr][i] + " ");
+                c++;
+            }
+            minr++;
+            for (int i = minr; i <= maxr; i++) {
+                System.out.print(arr[i][maxc] + " ");
+                c++;
+            }
+            minc--;
+            for (int i = maxc; i >= minc; i--) {
+                System.out.print(arr[maxr][i] + " ");
+                c++;
+            }
+            maxr--;
+            for (int i = maxr; i >= minr; i--) {
+                System.out.print(arr[i][minc] + " ");
+                c++;
+            }
+            minc++;
         }
-        minr++;
-        for(int i=minr;i<=maxr;i++){
-            System.out.println(arr[i][maxc]+" ");
-        }
-        minc--;
-        for(int i=maxc;i<=minc;i--){
-            System.out.println(arr[maxr][i]+" ");
-        }
-        maxr--;
-        for (int i=maxr;i>=minr;i--){
-            System.out.println(arr[i][minc]+" ");
-        }
-        minc++;
     }
 }
