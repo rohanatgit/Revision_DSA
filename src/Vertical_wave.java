7f220cc95c0b702962773c5121bc48a6663690c9
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-07-28 13:33:24 +0530

    Vertical_wave

---
+++ src/Vertical_wave.java
@@ -0,0 +1,8 @@
+public class Vertical_wave {
+    public static void main(String[] args) {
+        int arr[][]={{1,2,3,4},{5,6,7,8},{10,12,13,15}};
+    }
+    public static void print(int[] arr){
+
+    }
+}
