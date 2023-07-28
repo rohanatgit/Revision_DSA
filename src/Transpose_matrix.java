8354d33cd0d7bdfc8576aa9bb43affd02fe3ed9c
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-07-28 14:09:42 +0530

    Transpose_matrix

--- src/Transpose_matrix.java
+++ src/Transpose_matrix.java
@@ -1,6 +1,6 @@
 public class Transpose_matrix {
     public static void main(String[] args) {
-       int arr[][]={{2,3,4,7},{8,12,15,16},{18,1,2,4},{20,5,6,7}};
+       int arr[][]={{1,2,3},{4,5,6},{7,8,9}};
        transpose(arr);
     }
     public static void transpose(int arr[][]){
@@ -14,7 +14,7 @@ public class Transpose_matrix {
         }
         for(int i=0;i<arr.length;i++){
             for(int j=0;j<arr.length;j++){
-                System.out.println(arr[i][j]);
+                System.out.print(arr[i][j]);
             }
         }
     }
