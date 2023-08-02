6a2e2634668b2fa90bc19fac285356cbc58c6064
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-08-02 18:09:16 +0530

    String

--- src/String/Palindrome.java
+++ src/String/Palindrome.java
@@ -2,6 +2,19 @@ package String;
 
 public class Palindrome {
     public static void main(String[] args) {
+       String str="nitin";
 
+    }
+    public static boolean pal(String s){
+        int i=0;
+        int j=s.length()-1;
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
     }
 }
