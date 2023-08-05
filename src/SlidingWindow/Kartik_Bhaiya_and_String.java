45422faed42055d45b57ab3142f6cca6511c8637
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-08-05 11:46:42 +0530

    Sliding_Window

--- src/SlidingWindow/Kartik_Bhaiya_and_String.java
+++ src/SlidingWindow/Kartik_Bhaiya_and_String.java
@@ -2,6 +2,22 @@ package SlidingWindow;
 
 public class Kartik_Bhaiya_and_String {
     public static void main(String[] args) {
+      String s="abba";
+      int k=2;
+    }
+    public static int maximumlength(String s,int k){
+        int si=0;
+        int ei=0;
+        while(ei<s.length()){
 
+            //window grow
+
+
+            //window small
+
+
+            //window calculate
+
+        }
     }
 }
