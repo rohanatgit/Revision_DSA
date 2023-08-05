0db2685fe02c701748c07125f02e007db5d3cc11
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-08-05 11:48:53 +0530

    Sliding_Window

--- src/SlidingWindow/Kartik_Bhaiya_and_String.java
+++ src/SlidingWindow/Kartik_Bhaiya_and_String.java
@@ -4,8 +4,11 @@ public class Kartik_Bhaiya_and_String {
     public static void main(String[] args) {
       String s="abba";
       int k=2;
+      int flipa=maximumlength(s,k,'a');
+        int flipb=maximumlength(s,k,'b');
+
     }
-    public static int maximumlength(String s,int k){
+    public static int maximumlength(String s,int k,char ch){
         int si=0;
         int ei=0;
         while(ei<s.length()){
