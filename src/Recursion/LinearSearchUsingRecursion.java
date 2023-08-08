7c2699cffc381973424034bc9652ac0245882d43
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-08-08 09:45:17 +0530

    Recursion

--- src/Recursion/LinearSearchUsingRecursion.java
+++ src/Recursion/LinearSearchUsingRecursion.java
@@ -2,6 +2,15 @@ package Recursion;
 
 public class LinearSearchUsingRecursion {
     public static void main(String[] args) {
-
+       int arr[]={1,2,3};
+       int x=2;
+        System.out.println(linear(arr,0,x));
     }
+    public static int linear(int arr[],int i,int x){
+        if(arr[i]==x){
+            return i;
+        }
+        return linear(arr,i+1,x);
+    }
+
 }
