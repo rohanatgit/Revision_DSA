ca7cf627a13eb04d6e17dcf2fe57ffa08c55811b
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-08-08 09:39:16 +0530

    Recursion

---
+++ src/Recursion/print.java
@@ -0,0 +1,16 @@
+package Recursion;
+
+public class print {
+    public static void main(String[] args) {
+        int n=5;
+        print(n);
+    }
+    public static void print(int n){
+      if(n==0){
+          return ;
+      }
+
+        System.out.println(n);
+        print(n-1);
+    }
+}
