fd6d8487008fb5340355d11474571c3f2af08bc1
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-08-09 09:40:12 +0530

    Recursion

--- src/Recursion/coinTossWithoutTwoHeads.java
+++ src/Recursion/coinTossWithoutTwoHeads.java
@@ -3,7 +3,7 @@ package Recursion;
 public class coinTossWithoutTwoHeads {
     public static void main(String[] args) {
         int n=3;
-
+     printcoint(n,"");
     }
     public static void printcoint(int n,String ans){
         if(n==0){
