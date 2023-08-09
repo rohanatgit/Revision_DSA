4b4ebff43501357054fd45414725546b9b3de2c4
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-08-09 16:14:18 +0530

    Recursion

--- src/Recursion/generateParanthesis.java
+++ src/Recursion/generateParanthesis.java
@@ -1,20 +1,25 @@
 package Recursion;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class generateParanthesis {
     public static void main(String[] args) {
-
+         int n=3;
+         List<String> list=new ArrayList<>();
+        paranthesis(n,0,0,"",list);
     }
-    public static void paranthesis(int n,int opennig,int closing,String ans){
+    public static void paranthesis(int n,int opennig,int closing,String ans, List<String> list){
         if(opennig==n && closing==n){
-            System.out.println(ans);
+            list.add(ans);
             return ;
         }
         if(opennig<n)
         {
-            paranthesis(n,opennig+1,closing,ans+"(");
+            paranthesis(n,opennig+1,closing,ans+"(",list);
         }
         if(closing<opennig){
-            paranthesis(n,opennig,closing+1,ans+")");
+            paranthesis(n,opennig,closing+1,ans+")",list);
         }
     }
 }
