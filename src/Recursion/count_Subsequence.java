29015c23fe23425900891b1ac8c56693e243e50f
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-08-09 08:55:45 +0530

    Recursion

---
+++ src/Recursion/count_Subsequence.java
@@ -0,0 +1,17 @@
+package Recursion;
+
+public class count_Subsequence {
+    public static void main(String[] args) {
+        String str="abc";
+        printsubsequnec(str,"");
+    }
+    public static void printsubsequnec(String ques,String ans){
+        if(ques.length()==0){
+            System.out.println(ans);
+            return ;
+        }
+        char ch=ques.charAt(0);
+        printsubsequnec(ques.substring(1),ans);
+        printsubsequnec(ques.substring(1),ans+ch);
+    }
+}
