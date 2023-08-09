b9e2c811fa2517be334df56038096df2af20acc7
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-08-09 15:06:17 +0530

    Recursion

--- src/Recursion/BoardPath.java
+++ src/Recursion/BoardPath.java
@@ -2,6 +2,20 @@ package Recursion;
 
 public class BoardPath {
     public static void main(String[] args) {
-
+        int n=4;
+        System.out.println("\n"+boardpath(n,0,""));
+    }
+    public static int boardpath(int end,int curr,String ans){
+        if(curr==end){
+            System.out.println(ans+" ");
+            return 1;
+        }
+        if(curr>end){
+            return 0;
+        }
+        int fp=boardpath(end,curr+1,ans+1);
+        int sp=boardpath(end,curr+2,ans+2);
+        int tp=boardpath(end,curr+3,ans+3);
+        return fp+sp+tp;
     }
 }
