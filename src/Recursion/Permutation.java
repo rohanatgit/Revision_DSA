4fe92de2c001128a480864633c25a75dfaa94bb2
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-08-10 11:50:20 +0530

    Recursion

--- src/Recursion/Permutation.java
+++ src/Recursion/Permutation.java
@@ -2,6 +2,12 @@ package Recursion;
 
 public class Permutation {
     public static void main(String[] args) {
-
+       String str="abc";
+       printPermutation(str,"");
+    }
+    public static void printPermutation(String ques,String ans){
+        for(int i=0;i<ques.length();i++){
+            printPermutation(ques,);
+        }
     }
 }
