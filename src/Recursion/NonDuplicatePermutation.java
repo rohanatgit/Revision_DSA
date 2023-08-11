9d70561a441af154844b33326f12ad6c9b97a2d6
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-08-11 12:54:08 +0530

    Recursion

---
+++ src/Recursion/NonDuplicatePermutation.java
@@ -0,0 +1,26 @@
+package Recursion;
+
+public class NonDuplicatePermutation {
+    public static void main(String[] args) {
+
+    }
+    public static void printPermutation(String ques,String ans){
+        if(ques.length()==0){
+            System.out.println(ans);
+            return ;
+        }
+        for(int i=0;i<ques.length();i++) {
+            char ch = ques.charAt(i);
+            boolean flag = true;
+            for (int j = i; j < ques.length(); j++) {
+                if (ques.charAt(j) == ch) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag == true) {
+                String ros = ques.substring(0, i) + ques.substring(i + 1);
+            }
+        }
+    }
+}
