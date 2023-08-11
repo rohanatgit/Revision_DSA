8d9a56c2e6d7676acfebdad43b1b9652bd78bfd9
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-08-11 18:39:20 +0530

    Recursion

--- src/Recursion/keypadNOKIA.java
+++ src/Recursion/keypadNOKIA.java
@@ -1,5 +1,11 @@
 package Recursion;
 
 public class keypadNOKIA {
+    static String[] key={"","abc","def","ghi","jkl","mno","pqr","stu","vwx","yz"};
+    public  static void main(String[] args) {
+    }
+    public static void keyPad(String ques,String ans){
+        char ch=ques.charAt(0);
 
+    }
 }
