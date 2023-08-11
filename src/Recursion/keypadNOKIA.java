7cd6980b755a49fb47efc8f073343fbbe8513ec8
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-08-11 18:42:42 +0530

    Recursion

--- src/Recursion/keypadNOKIA.java
+++ src/Recursion/keypadNOKIA.java
@@ -5,7 +5,14 @@ public class keypadNOKIA {
     public  static void main(String[] args) {
     }
     public static void keyPad(String ques,String ans){
-        char ch=ques.charAt(0);
-
+        if(ques.length()==0){
+            System.out.print(ans);
+            return ;
+        }
+        char ch=ques.charAt(0);//'1'-->49
+        String pressString=key[ch-48];//ch-->49
+        for(int i=0;i<pressString.length();i++){
+            keyPad(ques,ans+pressString.charAt(i));
+        }
     }
 }
