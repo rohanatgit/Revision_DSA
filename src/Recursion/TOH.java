503a28304d0c638b948aad3ae14d5811be859c63
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-08-12 22:08:48 +0530

    Recursion

---
+++ src/Recursion/TOH.java
@@ -0,0 +1,13 @@
+package Recursion;
+
+public class TOH {
+    public static void main(String[] args) {
+        int n=3;
+        printstep(n,"src","hel","dis");
+    }
+
+    private static void printstep(int n, String s, String h,String d) {
+
+    }
+
+}
