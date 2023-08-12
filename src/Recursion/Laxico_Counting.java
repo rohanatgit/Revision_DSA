67bd9ea6e802e0f2d9b396b4f6863d26d8686400
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-08-12 18:28:32 +0530

    Recursion

--- src/Recursion/Laxico_Counting.java
+++ src/Recursion/Laxico_Counting.java
@@ -3,6 +3,15 @@ package Recursion;
 public class Laxico_Counting {
     public static void main(String[] args) {
         int n=1000;
+        printcounting(0,n);
+    }
+    public static void printcounting(int curr,int n){
+        int i=0;
+        if(curr==0){
+            i=1;
+        }
+        for(;i<=9;i++){
 
+        }
     }
 }
