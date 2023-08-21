f6f749ea65bb34099cffc1f33f7aad101cb1ddc6
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-08-21 20:18:06 +0530

    Recursion Backtracking

--- src/Recursion/Backtracking/RatChasesitscheese.java
+++ src/Recursion/Backtracking/RatChasesitscheese.java
@@ -7,6 +7,13 @@ public class RatChasesitscheese {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         int m=sc.nextInt();
-
+        char[][] maze=new char[n][m];
+        for(int i=0;i<maze.length;i++){
+           String s=sc.next();
+           for(int j=0;j<s.length();j++){
+               maze[i][j]=s.charAt(j);
+           }
+        }
     }
+
 }
