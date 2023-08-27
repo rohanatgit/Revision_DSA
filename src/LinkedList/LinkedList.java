ef9ac01992105fd0235c7c80f8730410820d9a6c
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-08-27 16:13:20 +0530

    Stack

--- src/LinkedList/LinkedList.java
+++ src/LinkedList/LinkedList.java
@@ -1,9 +1,15 @@
 package LinkedList;
 
 public class LinkedList {
-  public class Node{
+  public class Node {
       int data;
       Node next;
+
+      public Node() {
+          this.data = data;
+      }
   }
+  private Node head;
+  private int size;
 
 }
