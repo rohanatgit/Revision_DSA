3e28f3ba4c2e6b0d7e0f542072d73970ab5124ff
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-08-27 16:25:24 +0530

    Stack

--- src/LinkedList/LinkedList.java
+++ src/LinkedList/LinkedList.java
@@ -5,11 +5,22 @@ public class LinkedList {
       int data;
       Node next;
 
-      public Node() {
+      public Node(int data) {
           this.data = data;
       }
   }
   private Node head;
+  private Node tail;
   private int size;
+    public void addfirst(int item){
+        Node nn=new Node(item);
+        if(this.size==0){
+            this.head=nn;
+            this.tail=nn;
+            this.size++;
+        }
+        else{
 
+        }
+    }
 }
