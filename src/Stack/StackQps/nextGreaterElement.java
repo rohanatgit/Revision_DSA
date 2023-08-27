1921f4bf8a50f18966eee47616ce3520485924f9
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-08-27 11:23:15 +0530

    Stack

--- src/Stack/StackQps/nextGreaterElement.java
+++ src/Stack/StackQps/nextGreaterElement.java
@@ -2,6 +2,9 @@ package Stack.StackQps;
 
 public class nextGreaterElement {
     public static void main(String[] args) {
-        
+        int arr[]={11,3,23,9,15};
+    }
+    public static void NGE(int [] arr){
+
     }
 }
