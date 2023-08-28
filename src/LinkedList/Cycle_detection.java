c6932797aa32e557609ddacae0460203538daad7
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-08-28 18:27:52 +0530

    LinkedList

---
+++ src/LinkedList/Cycle_detection.java
@@ -0,0 +1,27 @@
+package LinkedList;
+
+public class Cycle_detection {
+    class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data=data;
+        }
+    }
+    private Node head;
+    private int size;
+    private Node tail;
+    public void addFirst(int item){
+        Node nn =new Node(item);
+        if(this.size==0){
+            this.head=nn;
+            this.tail=nn;
+            this.size++;
+        }
+        else{
+            nn.next=this.head;
+            this.head=nn;
+            this.size++;
+        }
+    }
+}
