243f0d805b3701b636c6c092f16791d0d5232e02
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-08-28 15:59:21 +0530

    LinkedList

--- src/LinkedList/LinkedList.java
+++ src/LinkedList/LinkedList.java
@@ -33,12 +33,64 @@ public class LinkedList {
         }
         System.out.println(".");
     }
+    public void addLast(int item){
+        if(this.size==0){
+            addfirst(item);
+        }
+        else{
+            Node nn=new Node(item);
+            this.tail.next=nn;
+            this.tail=nn;
+            this.size++;
+        }
+    }
+
+    //get
+    public int getFirst() throws Exception {
+        if(this.head==null){
+            throw new Exception("pgl LL khali hai ");
+        }
+        return this.head.data;
+    }
+
+    public int getlast() throws Exception {
+        if(head==null){
+            throw new Exception("pgl hai ya LL khali hai ");
+        }
+        return this.tail.data;
+    }
+
+  public int getatindex(int k) throws Exception {
+        if(k==0){
+            return getFirst();
+        }
+        else if(k==this.size-1){
+            return getlast();
+        }
+        else {
+            return getnode(k).data;
+        }
+  }
+
+
+  private Node getnode(int k) throws Exception {
+        if(k<0 || k>=size){
+            throw new Exception("idex out of bound pgl k ki value thoda shi de");
+        }
+        Node temp=head;
+        for(int i=1;i<=k;i++){
+            temp=temp.next;
+        }
+        return temp;
+  }
 
     public static void main(String[] args) {
         LinkedList ll=new LinkedList();
         ll.addfirst(10);
         ll.addfirst(20);
         ll.addfirst(30);
+        ll.Display();
+        ll.addLast(90);
         ll.Display();
     }
 }
