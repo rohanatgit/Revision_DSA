545eb2f6ae653d87ebcb531025a92bf084b86408
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-08-28 18:14:06 +0530

    LinkedList

--- src/LinkedList/Middle_of_LL.java
+++ src/LinkedList/Middle_of_LL.java
@@ -8,5 +8,15 @@ public class Middle_of_LL {
             this.data=data;
         }
     }
-
+    class Solution{
+        public Node middleNode(Node head){
+            Node fast=head;
+            Node slow=head;
+            while(fast!=null && fast.next!=null){
+                fast=fast.next.next;
+                slow=slow.next;
+            }
+            return slow;
+        }
+    }
 }
