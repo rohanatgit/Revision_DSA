c8de735959a64f2e0c39030f39aa5b5c4771e57a
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-08-30 15:49:30 +0530

    LinkedList

--- src/LinkedList/IntersentionOfTwoLL.java
+++ src/LinkedList/IntersentionOfTwoLL.java
@@ -1,5 +1,30 @@
 package LinkedList;
 
 public class IntersentionOfTwoLL {
-
+    class Node{
+        int data;
+       Node next;
+        public Node(int data){
+            this.data=data;
+        }
+    }
+    public Node Intersection(Node headA,Node headB){
+        Node A=headA;
+        Node B=headB;
+        while(A!=B){
+            if(A==null){
+                A=headB;
+            }
+            else{
+                A=A.next;
+            }
+            if(B==null){
+                B=headA;
+            }
+            else {
+                B=B.next;
+            }
+        }
+        return A;
+    }
 }
